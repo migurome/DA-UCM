f3bc98e453839ba739f2b91f585984fbab7cce4f
Miguel <migurome@ucm.es>
2017-10-04 10:24:22 +0200

    Modificaciones para una correcta entrega, incluyendo doxigen

--- 01.cpp
+++ 01.cpp
@@ -1,9 +1,13 @@
+// Grupo DA09, Miguel Romero Martínez
 
 #include <algorithm>
 #include <iostream>
 #include <fstream>
 #include "bintree_eda.h"
-// lee un árbol binario de la entrada estándar
+
+/**
+ * Lee un árbol binario de la entrada estándar
+ */
 
 template <typename T>
 bintree<T> leerArbol(T vacio) {
@@ -21,12 +25,29 @@ bintree<T> leerArbol(T vacio) {
 	}
 }
 
+/**
+ * Función secundaria.
+ * Complejidad O(n) siendo n el número de nodos.
+ * @param arbol que se quiere analizar
+ * @return  altura maxima de las ramas del arbol
+ */
+
 unsigned int altura(bintree<char> const& arbol) {
 	if (arbol.empty())
 		return 0;
 	else
 		return 1 + std::max(altura(arbol.left()), altura(arbol.right()));
 }
+
+/**
+ * Función principal. Si la raiz es vacía no hay nada que hacer. En otro caso,
+ * comprobamos que la raiz está entre el valor mínimo y máximo (inicializados como el máximo  y mínimo 
+ * valor posible de una variable int), y si es así comprobamos que la diferencia de alturas de las dos ramas
+ * cumple la condición para ser balanceado. En caso de que alguna de estas condiciones no se cumpla, 
+ * sabemos que el árbol no es AVL.  Complejidad O(n) siendo n el número de nodos.
+ * @param arbol que se quiere analizar
+ * @return si el arbol es equilibrado o no
+ */
 
 bool equilibrado(bintree<char> const& arbol) {
 	 
@@ -41,18 +62,17 @@ bool equilibrado(bintree<char> const& arbol) {
 		altura_I = altura(arbol.left());
 		altura_D = altura(arbol.right());
 
-		if (std::abs(altura_I - altura_D) <= 1)
-			return true;
-		else
-			return false;
+		return std::abs(altura_I - altura_D) <= 1;
+
 	}
 	else
-		return false;
-	
-	
+		return false;	
 }
 
-// resuelve un caso de prueba
+/**
+ * Resuelve un caso de prueba
+ */
+
 void resuelveCaso() {
 		
 	auto arbol = leerArbol('.');
@@ -73,7 +93,6 @@ int main(int argc, char* argv[]) {
 
 	int numCasos;
 	std::cin >> numCasos;
-	std::cin.ignore(256, '\n');
 
 	for (int i = 0; i < numCasos; ++i)
 		resuelveCaso();
